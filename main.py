53c9a192d21cd8c306b6afdcc0490e13e37b49c4
Gabimcclure <54961341+gabimcclure@users.noreply.github.com>
2019-09-20 11:46:11 -0700

    float layout

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-
-class MyGrid(Widget):
-    pass
+from kivy.uix.floatlayout import FloatLayout
 
 class MyApp(App):
     def build(self):
-        return MyGrid()
+        return FloatLayout()
 
 
 if __name__ == "__main__":
